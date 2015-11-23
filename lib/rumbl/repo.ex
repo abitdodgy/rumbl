5e3e5c43e936dcb1f8ce43fa542580465e1c5d8d
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-11-23 21:27:02 -0200

    chapter 3

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-
-  @moduledoc """
-  In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [ %Rumbl.User{id: "1", name: "Mohamad El-Husseini", username: "abitdodgy", password: "abitdodgy"},
-      %Rumbl.User{id: "2", name: "Sami El-Husseini", username: "sami", password: "sami"},
-      %Rumbl.User{id: "3", name: "Dani El-Husseini", username: "dani", password: "dani"}]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map ->
-      map.id == id
-    end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, value} ->
-        Map.get(map, key) == value
-      end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
